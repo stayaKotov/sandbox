6dba688a80a3fba19cd506211e341ac84be87d91
Savinkov Anton Vladimirovich <ansavinkov@ozon.ru>
2021-05-15 23:40:14 +0300

    [IS-002] create merge_sort

---
+++ algos/sorting/merge_sort.py
@@ -0,0 +1,38 @@
+import typing as t
+
+
+class MergeSort:
+    def __init__(self, elements: t.List):
+        self.elements = elements
+
+    def _split(self, elems):
+        middle = len(elems) // 2
+        return elems[:middle], elems[middle:]
+
+    def _merge(self, left, right):
+        result_arr = []
+        it1, it2 = 0, 0
+        while it1 < len(right) and it2 < len(left):
+            if right[it1] < left[it2]:
+                result_arr.append(right[it1])
+                it1 += 1
+            else:
+                result_arr.append(left[it2])
+                it2 += 1
+        if it1 == len(right) and it2 < len(left):
+            result_arr.extend(left[it2:])
+        elif it2 == len(left) and it1 < len(right):
+            result_arr.extend(right[it1:])
+        return result_arr
+
+    def _wrapper(self, els):
+        if len(els) > 1:
+            left, right = self._split(els)
+            sorted_left = self._wrapper(left)
+            sorted_right = self._wrapper(right)
+            return self._merge(sorted_left, sorted_right)
+        else:
+            return els
+
+    def sort(self):
+        return self._wrapper(self.elements)
--- algos/sorting/test.py
+++ algos/sorting/test.py
@@ -1,6 +1,7 @@
 from bubble_sort import BubbleSort
 from insert_sort import InsertSort
 from selection_sort import SelectionSort
+from merge_sort import MergeSort
 import numpy as np
 
 
@@ -34,7 +35,20 @@ def run_insert_sort():
     print()
 
 
+def run_merge_sort():
+    ll = np.random.randint(-10, 10, 10)#[7, 6, 5, 4, 3, 2, 1]
+    print(ll)
+    _is = MergeSort(ll)
+    res = _is.sort()
+    print("MergeSort")
+    print
+    print(f"before: {ll}")
+    print(f"after: {res}")
+    print()
+
+
 if __name__ == "__main__":
-    run_bubble_sort()
-    run_selection_sort()
-    run_insert_sort()
+    # run_bubble_sort()
+    # run_selection_sort()
+    # run_insert_sort()
+    run_merge_sort()
